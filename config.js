4f459b36ac37445692ca3eebefd2379507ea958e
Ulrik Hansen <ulrik-ulle@hotmail.com>
2020-11-02 10:48:02 +0100

    prøver bare

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+const config = {
+    databaseURI: 'mongodb+srv://Ulrik:jjg73fnz@cluster0.k8pf9.mongodb.net/Joke?retryWrites=true&w=majority',
+    localPort: 8080
+};
+
+module.exports = config;
