89c927f6a75ca5f9831997b4bb902502304b070b
Ulrik Hansen <ulrik-ulle@hotmail.com>
2020-11-04 09:13:31 +0100

    kæft det kører

--- public/index.js
+++ public/index.js
@@ -6,6 +6,17 @@ async function get(url) {
     return await respons.json();
 }
 
+async function post(url, objekt) {
+    const respons = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(objekt),
+        headers: { 'Content-Type': 'application/json' }
+    });
+    if (respons.status !== 201) // Created
+        throw new Error(respons.status);
+    return await respons.json();
+}
+
 async function getText(url) {
     const respons = await fetch(url);
     if (respons.status !== 200) // OK
@@ -22,30 +33,53 @@ async function generateJokesTable(jokes) {
 async function main() {
     try {
         let jokes = await get('/joke/api/jokes');
-        let div = document.getElementById("container")
-        div.innerHTML = await generateJokesTable(jokes)
+        let div = document.getElementById('jokesDiv')
+        div.innerHTML = await generateJokesTable(jokes);
     } catch (e) {
         console.log(e.name + ": " + e.message);
     }
 }
 main();
 
-async function post(url, objekt) {
-    const respons = await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(objekt),
-        headers: { 'Content-Type': 'application/json' }
-    });
-    if (respons.status !== 201) // Created
-        throw new Error(respons.status);
-    return await respons.json();
+let rydButton = document.getElementById('clear')
+let opretButton = document.getElementById('opretButton')
+
+opretButton.onclick = async () => {
+    try {
+        await post("/joke/api/jokes", { setup: setup.value, punchline: punchline.value });
+        
+    } catch (e) {
+    }
+    setup.value = ''
+    punchline.value = ''
 }
 
-let knap = document.getElementById("Knap");
-knap.onclick = async () => {
+rydButton.onclick = async () => {
+    setup.value = ''
+    punchline.value = ''
+}
+let selectSite = document.getElementById('selectSite')
+
+
+async function getSites() {
     try {
-        await post("/joke/api/jokes", { setup: 'setup.value', punchline: 'punchline.value' })
-    } catch (e) {
+        let result = await get('https://krdo-joke-registry.herokuapp.com/api/services');
+        createSelect(result)
+    }
+    catch (e) {
         console.log(e);
     }
 }
+
+getSites()
+
+async function generateSelect() {
+    try {
+        let sites = await get('https://krdo-joke-registry.herokuapp.com/api/services')
+        return sites
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
+
